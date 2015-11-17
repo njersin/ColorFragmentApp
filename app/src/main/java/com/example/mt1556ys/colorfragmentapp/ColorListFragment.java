e2e01709fa2c3e324265b8a1a05420d74dcc70fc
mt1556ys <jnguetio@gmail.com>
2015-11-16 18:05:23 -0600

    Initial commit

---
+++ app/src/main/java/com/example/mt1556ys/colorfragmentapp/ColorListFragment.java
@@ -0,0 +1,58 @@
+package com.example.mt1556ys.colorfragmentapp;
+
+import android.app.Activity;
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class ColorListFragment extends Fragment {
+
+    private ListView mColorListView;
+    private ArrayAdapter<String> mColorArray;
+
+    private OnListItemSelectedListener mListItemSelectedListener;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup group, Bundle savedInstanceState) {
+
+        View v = inflater.inflate(R.layout.color_list_fragment, group, false);
+        mColorListView = (ListView) v.findViewById(R.id.color_list_view);
+
+        mColorListView.setAdapter(mColorArray);
+        mColorListView.setChoiceMode(ListView.CHOICE_MODE_SINGLE);
+        mColorListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                mListItemSelectedListener.onListItemSelected(mColorArray.getItem(position));
+            }
+        });
+
+        return v;
+    }
+
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+
+        mColorArray = new ArrayAdapter<>(this.getActivity(), R.layout.color_list_view_item, R.id.list_item);
+        mColorArray.add("Blue");
+        mColorArray.add("Green");
+
+        try {
+            mListItemSelectedListener = (OnListItemSelectedListener) activity;
+        } catch (ClassCastException cce) {
+            throw new ClassCastException(activity.toString() + " must implement OnListItemSelectedListener.");
+        }
+    }
+
+
+    public interface OnListItemSelectedListener {
+        void onListItemSelected(String colorItem);
+    }
+
+}
---
+++ app/src/main/java/com/example/mt1556ys/colorfragmentapp/MainActivity.java
@@ -0,0 +1,78 @@
+package com.example.mt1556ys.colorfragmentapp;
+
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity implements ColorListFragment.OnListItemSelectedListener  {
+
+    private ColorListFragment mColorListFragment;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mColorListFragment = new ColorListFragment();
+
+        FragmentManager manager = getFragmentManager();
+        FragmentTransaction transaction = manager.beginTransaction();
+        transaction.add(android.R.id.content, mColorListFragment);
+        transaction.commit();
+    }
+
+    @Override
+    public void onListItemSelected(String colorItem) {
+
+        FragmentManager manager = getFragmentManager();
+        FragmentTransaction transaction = manager.beginTransaction();
+
+        if (colorItem == "Blue") {
+            BlueFragment itemFragment = new BlueFragment();
+            transaction.replace(android.R.id.content, itemFragment);
+
+        } else if (colorItem == "Green") {
+            GreenFragment itemFragment = new GreenFragment();
+            transaction.replace(android.R.id.content, itemFragment);
+        }
+
+        transaction.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
+        transaction.addToBackStack(null);
+        transaction.commit();
+    }
+
+    @Override
+    public void onBackPressed() {
+        FragmentManager manager = getFragmentManager();
+        manager.popBackStack();
+        FragmentTransaction transaction = manager.beginTransaction();
+        transaction.replace(android.R.id.content, mColorListFragment);
+        transaction.commit();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+}
